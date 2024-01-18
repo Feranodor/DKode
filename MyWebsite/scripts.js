2318ac0005bfc724706bf408d41abb21b02e0358
Piotr Kraska <piotrkraska88@wp.pl>
2024-01-18 20:57:50 +0100

    add tryEvaluate

--- MyWebsite/scripts.js
+++ MyWebsite/scripts.js
@@ -5,13 +5,19 @@ const text = document.querySelector('input[type="text"]');
 const buttons = document.querySelectorAll('input[type="button"]');
 buttons.forEach(x => x.onclick = () => execute(x.value));
 
+const incorrect = 'incorrect expression';
+
 async function execute(v) {
+    if (text.value === incorrect) {
+        text.value = '';
+    }
+
     switch (v) {
         case 'C':
             text.value = '';
             break;
         case '=':
-            text.value = math.evaluate(text.value);
+            text.value = tryEvaluate(text.value);
             break;
         case 'GBP':
         case 'EUR':
@@ -19,9 +25,19 @@ async function execute(v) {
             const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${v}/2023-12-22/?format=json`);
             const json = await response.json();
             const rate = json.rates[0].mid;
-            text.value = math.evaluate(text.value + '*' + rate);
+            text.value = tryEvaluate(text.value + '*' + rate);
             break;
         default:
             text.value += v;
     };
+}
+
+function tryEvaluate(expession) {
+    let result;
+    try {
+        result = math.evaluate(expession);
+    } catch (error) {
+        result = incorrect;
+    }
+    return result;
 }
