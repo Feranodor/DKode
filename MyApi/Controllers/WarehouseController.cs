c2369838f65ab56d2d78c9be9930e1537a33e46a
Piotr Kraska <piotrkraska88@wp.pl>
2024-01-20 23:41:24 +0100

    saving csvs

--- MyApi/Controllers/WarehouseController.cs
+++ MyApi/Controllers/WarehouseController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System;
+using static System.Net.WebRequestMethods;
+using Microsoft.AspNetCore.Routing.Constraints;
 
 namespace MyApi.Controllers
 {
@@ -7,13 +11,53 @@ namespace MyApi.Controllers
     [ApiController]
     public class WarehouseController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+        static readonly HttpClient client = new HttpClient();
+        static readonly string[] filePaths =
         {
+            "https://rekturacjazadanie.blob.core.windows.net/zadanie/Products.csv",
+            "https://rekturacjazadanie.blob.core.windows.net/zadanie/Inventory.csv",
+            "https://rekturacjazadanie.blob.core.windows.net/zadanie/Prices.csv"
+        };
+
+
+        [HttpGet("firstStep")]
+        public async Task<IActionResult> Get()
+        {
+            var uriPaths = filePaths.Select(x => new Uri(x))
+                .Select(uri => (uri: uri, path: $".\\{Path.GetFileName(uri.LocalPath)}"))
+                .ToArray();
+
+            var downloadTasks = uriPaths.Select(x =>
+                {
+                    if (System.IO.File.Exists(x.path))
+                    {
+                        return Task.CompletedTask;
+                    }
+                    return Download(x.path, x.uri);
+                })
+                .ToArray();
+
+            await Task.WhenAll(downloadTasks);
+
+
+
+
+
+
             return Ok();
         }
 
-        [HttpGet]
+        private async Task Download(string filePath, Uri uri)
+        {
+            using HttpResponseMessage response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            using Stream input = await response.Content.ReadAsStreamAsync();
+            using Stream output = System.IO.File.OpenWrite(filePath);
+            input.CopyTo(output);
+        }
+
+
+        [HttpGet("secondStep")]
         public IActionResult Get(string sku)
         {
             return Ok();
